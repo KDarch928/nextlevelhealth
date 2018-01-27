3c69fc688b0ff605f1740e183f7036710479da28
KDarch928 <Kayle.Drucker@gmail.com>
2018-01-27 12:30:57 -0600

    udpated html layout

--- assets/javascript/next.js
+++ assets/javascript/next.js
@@ -1,3 +1,20 @@
-$('.carousel').carousel({
-    interval: 2000
-})
+$("#new-usr").on("click", function (event) {
+    event.preventDefault();
+    $("#name-check").addClass("invisible");
+    $("#new-user").removeClass("invisible");
+
+});
+
+$("#join-btn").on("click", function (event) {
+    event.preventDefault();
+    $("#new-user").addClass("invisible");
+    $("#user-display").removeClass("invisible");
+    $("#user-food-display").removeClass("invisible");
+});
+
+$("#log-out").on("click", function (event) {
+    event.preventDefault();
+    $("#name-check").removeClass("invisible");
+    $("#user-display").addClass("invisible");
+    $("#user-food-display").addClass("invisible");
+});
