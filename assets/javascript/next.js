01b6f25c302c20a559a99d8827a871403541f12a
KDarch928 <Kayle.Drucker@gmail.com>
2018-01-30 19:18:52 -0600

    Made revisions to the buttons

--- assets/javascript/next.js
+++ assets/javascript/next.js
@@ -1,20 +1,72 @@
-$("#new-usr").on("click", function (event) {
-    event.preventDefault();
-    $("#name-check").addClass("invisible");
-    $("#new-user").removeClass("invisible");
+function searchRecipes() {
+    var queryURL = "https://api.edamam.com/search"
 
-});
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        dataType: "json",
+        data: {
+            "app_id": "ba334122",
+            "app_key": "559625546067efe571c6b8650185cc3d",
+            "q": "chicken",
+            "contentType": "application/json",
+        }
 
-$("#join-btn").on("click", function (event) {
-    event.preventDefault();
+    }).done(function (response) {
+        console.log(response);
+    });
+}
+
+
+
+// $("#new-usr").on("click", function (event) {
+//     event.preventDefault();
+//     $("#name-check").addClass("invisible");
+//     $("#new-user").removeClass("invisible");
+
+// });
+
+// $("#join-btn").on("click", function (event) {
+//     event.preventDefault();
+//     $("#new-user").addClass("invisible");
+//     $("#user-display").removeClass("invisible");
+//     $("#user-food-display").removeClass("invisible");
+// });
+
+// $("#log-out").on("click", function (event) {
+//     event.preventDefault();
+//     $("#name-check").removeClass("invisible");
+//     $("#user-display").addClass("invisible");
+//     $("#user-food-display").addClass("invisible");
+// });
+
+function userProfilePage() {
     $("#new-user").addClass("invisible");
+    $("#name-check").addClass("invisible");
     $("#user-display").removeClass("invisible");
     $("#user-food-display").removeClass("invisible");
-});
+}
 
-$("#log-out").on("click", function (event) {
-    event.preventDefault();
+function newUserPage() {
+    $("#name-check").addClass("invisible");
+    $("#new-user").removeClass("invisible");
+}
+
+function homePageDisplay() {
     $("#name-check").removeClass("invisible");
     $("#user-display").addClass("invisible");
     $("#user-food-display").addClass("invisible");
-});
+}
+
+$(".btn").on("click",function (event) {
+    event.preventDefault();
+    var btnType = $(this).val();
+    console.log(btnType);
+    if(btnType === "newUser") {
+        newUserPage();
+    } else if (btnType === "join" || btnType === "nameCheck"){
+        userProfilePage();
+    } else if (btnType === "logOut"){
+        homePageDisplay();
+    }
+})
